c797524fb917b778a39f16be7c04f08974e7083c
fuchenxu2008 <fuchenxu2008@163.com>
2019-05-03 01:50:11 +0800

    remove https

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,3 @@
-export const DOMAIN_URL = 'https://kyrie.top:4263';
+export const DOMAIN_URL = 'http://kyrie.top:4263';
 // export const DOMAIN_URL = 'http://localhost:4263';
 export const MapBoxToken = 'pk.eyJ1IjoiZnVjaGVueHUyMDA4IiwiYSI6ImNqbjRkMjN5bjVnNWQza3FjM2ZnNWIxbzkifQ.46R1gdQJU3-VaPCL0BHIyQ';
