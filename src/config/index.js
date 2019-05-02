23b35b86cd4f25f5b8354fdbb91416a729d5f13b
fuchenxu2008 <fuchenxu2008@163.com>
2019-05-03 00:59:07 +0800

    change domain_url

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,2 @@
-export const DOMAIN_URL = 'http://192.168.1.107:4263';
+export const DOMAIN_URL = 'http://localhost:4263';
 export const MapBoxToken = 'pk.eyJ1IjoiZnVjaGVueHUyMDA4IiwiYSI6ImNqbjRkMjN5bjVnNWQza3FjM2ZnNWIxbzkifQ.46R1gdQJU3-VaPCL0BHIyQ';
