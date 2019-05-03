5ff15a1b0880e7e810d5794dd66776d3562e766f
fuchenxu2008 <fuchenxu2008@163.com>
2019-05-03 17:08:33 +0800

    add env

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,2 @@
-export const DOMAIN_URL = 'http://kyrie.top:4263';
-// export const DOMAIN_URL = 'http://localhost:4263';
+export const DOMAIN_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:4263' : 'http://map.kyrie.top';
 export const MapBoxToken = 'pk.eyJ1IjoiZnVjaGVueHUyMDA4IiwiYSI6ImNqbjRkMjN5bjVnNWQza3FjM2ZnNWIxbzkifQ.46R1gdQJU3-VaPCL0BHIyQ';
