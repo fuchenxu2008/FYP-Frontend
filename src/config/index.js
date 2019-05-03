1ad2ff8918335ff0d04c08ec817bbad0481510e4
fuchenxu2008 <fuchenxu2008@163.com>
2019-05-03 17:19:13 +0800

    update env

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,2 @@
-export const DOMAIN_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:4263' : 'http://map.kyrie.top';
+export const DOMAIN_URL = process.env.NODE_ENV === 'production' ? 'http://map.kyrie.top' : 'http://localhost:4263';
 export const MapBoxToken = 'pk.eyJ1IjoiZnVjaGVueHUyMDA4IiwiYSI6ImNqbjRkMjN5bjVnNWQza3FjM2ZnNWIxbzkifQ.46R1gdQJU3-VaPCL0BHIyQ';
